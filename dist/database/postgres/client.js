5942c0a152c22ccb62d5578334c9866a9cd19e0a
DGTJS <joaodiegom15@gmail.com>
2026-07-27 08:56:48 -0300

    feat: set up PostgreSQL database connection and basic Express server

---
+++ dist/database/postgres/client.js
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+process.loadEnvFile?.(".env");
+export const pool = new Pool({
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+});
---
+++ dist/index.js
@@ -0,0 +1,19 @@
+import process from "node:process";
+import express from "express";
+import { pool } from "./database/postgres/client.js";
+process.loadEnvFile?.(".env");
+const app = express();
+app.listen(3000, async () => {
+    console.log("Server running on port 3000");
+    const client = await pool.connect();
+    if (client) {
+        console.log("Connected to database");
+        client.release();
+    }
+    else {
+        console.log("Failed to connect to database");
+    }
+});
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
